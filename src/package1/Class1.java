032ca71944a7772e1ca5beb6a60b971dcfbc50f2
Osman Turkarslan <osmanturkarslan1620@gmail.com>
2021-02-27 21:44:07 +0200

    fourth commit

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -5,6 +5,8 @@ public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		int  numberOfStudents = 71;
+		double num = 34.5;
+		// here some changes
 	}
 
 }
